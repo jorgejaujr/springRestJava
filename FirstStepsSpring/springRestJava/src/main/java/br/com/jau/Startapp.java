3764a5d8596ca99b0790af567e2eba4e8d75cd29
jorgejaujr <jorgejaujr@gmail.com>
2024-07-26 12:37:58 -0300

    First commit on java spring - Get Method implemented

---
+++ FirstStepsSpring/springRestJava/src/main/java/br/com/jau/Startapp.java
@@ -0,0 +1,13 @@
+package br.com.jau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startapp.class, args);
+	}
+
+}
---
+++ FirstStepsSpring/springRestJava/src/test/java/br/com/jau/SpringRestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
